02d7e438abea9198a1e97df14ed64a65981a83eb
Shingai Shamu <mshamu95@gmail.com>
2022-09-28 01:30:17 +0200

    added multiple changes
    1. Added Members and Labels
    2. Added Attachments
    3. Added additional fields

--- trello_blueprints/create_ticket.py
+++ trello_blueprints/create_ticket.py
@@ -1,5 +1,8 @@
 import argparse
 import sys
+import re
+from ast import literal_eval
+from datetime import datetime
 import requests
 import shipyard_utils as shipyard
 try:
@@ -25,13 +28,34 @@ def get_args():
     parser.add_argument('--list-name', dest='list_name', required=True)
     parser.add_argument('--start-date', dest='start_date', required=False)
     parser.add_argument('--due-date', dest='due_date', required=False)
+    parser.add_argument('--due-complete', dest='due_complete', required=False)
+    parser.add_argument('--members', dest='members_list', required=False)
+    parser.add_argument('--labels', dest='labels_list', required=False)    
+    parser.add_argument('--url-source', dest='url_source', required=False)
+    parser.add_argument('--address', dest='address', required=False)
+    parser.add_argument('--location-name', dest='location_name', required=False)
+    parser.add_argument('--coordinates', dest='coordinates', required=False)
+    parser.add_argument(
+        '--source-file-name',
+        dest='source_file_name',
+        required=False)
+    parser.add_argument(
+        '--source-folder-name',
+        dest='source_folder_name',
+        default='',
+        required=False)
+    parser.add_argument('--source-file-name-match-type',
+                        dest='source_file_name_match_type',
+                        choices={'exact_match', 'regex_match'},
+                        default='exact_match',
+                        required=False)
     args = parser.parse_args()
     return args
 
 
-def get_list_id_from_name(api_key, token, board_name, list_name):
-    """Gets the Trello List id given the Name of the List and the board it's from"""
-    get_url = f"https://api.trello.com/1/members/me/boards"
+def get_board_id_from_name(api_key, token, board_name):
+    """Gets the Trello board id given the board_name"""
+    get_url = "https://api.trello.com/1/members/me/boards"
     params = {
         'key': api_key,
         'token': token,
@@ -45,13 +69,65 @@ def get_list_id_from_name(api_key, token, board_name, list_name):
         trello_data = get_response.json()
         for board in trello_data:
             if board['name'] == board_name:
-                board_list = board['lists']
+                return board['id']
 
+
+def get_label_ids_from_name(api_key, token, board_id, labels):
+    """Gets the Trello Label id given a list of Label names and board"""
+    get_url = f"https://api.trello.com/1/boards/{board_id}/labels"
+    params = {
+        'key': api_key,
+        'token': token,
+    }
+    get_response = requests.get(get_url, params=params)
+    if get_response.status_code == 200:
+        labels_data = get_response.json()
+        # find and return the label
+        label_ids = [
+            label['id'] for label in labels_data if label['name'] in labels
+        ]
+        return label_ids
+        
+
+def get_member_ids_from_name(api_key, token, board_id, members):
+    """Gets the Trello Member id given a list of members names and board"""
+    get_url = f"https://api.trello.com/1/boards/{board_id}/memberships"
+    params = {
+        'key': api_key,
+        'token': token,
+    }
+    get_response = requests.get(get_url, params=params)
+    if get_response.status_code == 200:
+        member_data = get_response.json()
+        # find and return the label
+        member_ids = [
+            member['id'] for member in member_data if member['name'] in members
+        ]
+        return member_ids
+
+
+def get_list_id_from_name(api_key, token, board_id, list_name):
+    """Gets the Trello List id given the Name of the List and the board it's from"""
+    get_url = f"https://api.trello.com/1/boards/{id}/lists"
+    params = {
+        'key': api_key,
+        'token': token,
+    }
+    get_response = requests.get(get_url, params=params)
+
+    if get_response.status_code == requests.codes.ok:
+        # get the list data from json response
+        lists_data = get_response.json()
         # get list id from board lists
-        list_id = [b_list['id'] for b_list in board_list if b_list['name'] == list_name]
-
-        # return list id as a str
-        return list_id[0]
+        list_id = [
+            b_list['id'] for b_list in lists_data if b_list['name'] == list_name
+        ]
+        if list_id:
+            # return list id as a str
+            return list_id[0]
+        else:
+            print(f"Error: No list with name {list_name} found")
+            sys.exit(exit_codes.BAD_REQUEST)
 
     elif get_response.status_code == 401: # incorrect credentials
         print("You do not have the required permissions or wrong credentials")
@@ -66,7 +142,7 @@ def get_list_id_from_name(api_key, token, board_name, list_name):
 
 
 
-def create_ticket(api_key, token, id_list, name, description, start_date=None, due_date=None):
+def create_ticket(api_key, token, id_list, name, description, payload):
     """ Triggers the Create Card API and adds a new card
     https://developer.atlassian.com/cloud/trello/rest/api-group-cards/#api-cards-post
     """
@@ -84,15 +160,13 @@ def create_ticket(api_key, token, id_list, name, description, start_date=None, d
        'name': name,
        'desc': description
     }
-    if start_date:
-        query['start'] = start_date
-    if due_date:
-        query['due'] = due_date,
+    # add additional fields to the query
+    query.update(payload)
 
     response = requests.post(url,headers=headers, json=query)
 
     if response.status_code == 200:
-        print(f"Card created successfully")
+        print("Card created successfully")
         return response.json()
         
     elif response.status_code == 401: # Credentials Error
@@ -112,26 +186,111 @@ def create_ticket(api_key, token, id_list, name, description, start_date=None, d
         sys.exit(exit_codes.UNKNOWN_ERROR)
     
 
+def attach_file_to_card(api_key, token, card_id, file_path):
+    """ Attaches files to a Trello card."""
+
+    attachment_endpoint = f"https://api.trello.com/1/cards/{card_id}/attachments"
+    
+    headers = {
+       "Accept": "application/json"
+    }
+    params = {
+       'key': api_key,
+       'token': token
+    }
+    file_payload = {
+        "file": (file_path, open(file_path, "rb"), "application-type")
+    }
+    response = requests.post(attachment_endpoint,
+                             headers=headers,
+                             params=params,
+                             files=file_payload)
+
+    if response.status_code == 200:
+        print(f'{file_path} was successfully attached to {card_id}')
+    return response
+
+
+def convert_date_to_trello(shipyard_date):
+    """Converts date from shipyard input MM/DD/YYYY to 
+    ISO 8086 date plus Zulu (Z).
+    """ 
+    str_as_date = datetime.strptime(shipyard_date, '%m/%d/%Y')
+    converted_date = str_as_date.isoformat() + "Z"
+    return converted_date
+
+
 def main():
     args = get_args()
     api_key = args.api_key
     access_token = args.access_token
     board_name = args.board_name
     list_name = args.list_name
+    source_file_name = args.source_file_name
+    source_folder_name = args.source_folder_name
+    source_file_name_match_type = args.source_file_name_match_type
+
+    # get board_id for use in the rest of the functions
+    board_id = get_board_id_from_name(api_key, access_token, board_name)
     # get list id
     list_id = get_list_id_from_name(api_key, access_token, 
-                    board_name, list_name)
+                    board_id, list_name)
+    # get labels and members
+    label_ids = get_label_ids_from_name(api_key, access_token, 
+                                          board_id, literal_eval(args.labels))
+    member_ids = get_label_ids_from_name(api_key, access_token, 
+                                          board_id, literal_eval(args.members))
 
+    # create payload dict and add data to it
+    payload = {}
+    if args.start_date:
+        payload['start'] = convert_date_to_trello(args.start_date)
+    if args.due_date:
+        payload['due'] = convert_date_to_trello(args.due_date)  
+    if args.due_complete:
+        payload['dueComplete'] = args.due_complete
+    if member_ids:
+        payload['idMembers'] = member_ids
+    if label_ids:
+        payload['idLabels'] = label_ids
+    if args.url_source:
+        payload['urlSource'] = args.url_source
+    if args.address:
+        payload['address'] = args.address
+    if args.location_name:
+        payload['locationName'] = args.location_name
+    if args.coordinates:
+        payload['coordinates'] = args.coordinates
+
+    # create card
     card_data = create_ticket(
             api_key, 
             access_token, 
             list_id, 
             args.name, 
-            args.description, 
-            args.start_date, 
-            args.due_date
+            args.description,
+            payload=payload
     )
     card_id = card_data['id']
+    
+    # add attachments
+    if source_file_name_match_type == 'regex_match':
+        all_local_files = shipyard.files.find_all_local_file_names(
+            source_folder_name)
+        matching_file_names = shipyard.files.find_all_file_matches(
+            all_local_files, re.compile(source_file_name))
+        for index, file_name in enumerate(matching_file_names):
+            attach_file_to_card(api_key, 
+                                access_token, 
+                                card_id,
+                                file_name)
+    else:
+        source_file_path = shipyard.files.combine_folder_and_file_name(
+            source_folder_name, source_file_name)
+        attach_file_to_card(api_key, 
+                            access_token, 
+                            card_id,
+                            source_file_path)
     
     # save card to responses
     card_data_filename = shipyard.files.combine_folder_and_file_name(
